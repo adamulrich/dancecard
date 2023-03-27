38c18059507b78d1f7debb383660e82c83bfc04c
Adam Ulrich <adamulrich@hotmail.com>
2023-03-27 14:08:22 -0700

    wip

--- src/scripts/aside.js
+++ src/scripts/aside.js
@@ -1,1 +1,16 @@
-// if signed in, then show correct aside.
+import { getLocalStorage } from './utils.js';
+
+const userData = getLocalStorage();
+try {
+    if (userData) {
+        document.getElementById('signed-in').classList.remove('disabled');
+        document.getElementById('signed-out').classList.add('disabled');
+    } else {
+        document.getElementById('signed-out').classList.remove('disabled');
+        document.getElementById('signed-in').classList.add('disabled');
+    }
+        
+} catch (error) {
+    document.getElementById('signed-out').classList.remove('disabled');
+    document.getElementById('signed-in').classList.add('disabled');
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,12 +2,15 @@ import { getLocalStorage } from './utils.js';
 
 const userData = getLocalStorage();
 try {
-    if (userData.isRegionAdmin == true) {
-        document.getElementById('admin-link').classList.remove('disabled')
+    if (userData.regionAdmin == true) {
+        document.getElementById('admin-link').classList.remove('hidden');
+        document.getElementById('aside-admin-link').classList.remove('hidden');
     } else {
-        document.getElementById('admin-link').classList.add('disabled')    
+        document.getElementById('admin-link').classList.add('hidden');
+        document.getElementById('aside-admin-link').classList.add('hidden');
     }
         
 } catch (error) {
-    document.getElementById('admin-link').classList.add('disabled')
+    document.getElementById('admin-link').classList.add('hidden');
+    document.getElementById('aside-admin-link').classList.add('hidden');
 }
